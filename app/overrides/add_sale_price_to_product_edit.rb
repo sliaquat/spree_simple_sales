0ea7b1b5e58d9b510001a79d2491e1c82eff1cec
Amrta Faruqi <amrta@lizard-apps.com>
2015-01-20 11:59:13 +0800

    Remove asterisk from 'sale price' field

--- app/overrides/add_sale_price_to_product_edit.rb
+++ app/overrides/add_sale_price_to_product_edit.rb
@@ -3,7 +3,7 @@ Deface::Override.new(virtual_path: 'spree/admin/products/_form',
   insert_after: "erb[loud]:contains('text_field :price')",
   text: "
     <%= f.field_container :sale_price do %>
-      <%= f.label :sale_price, raw(Spree.t(:sale_price) + content_tag(:span, ' *')) %>
+      <%= f.label :sale_price, raw(Spree.t(:sale_price)) %>
       <%= f.text_field :sale_price, value: number_to_currency(@product.sale_price, unit: '') %>
       <%= f.error_message_on :sale_price %>
     <% end %>
