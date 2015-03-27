ac1aaee377de2e9b43a9af6866f5621d32f096e7
Sanad Liaquat <sanadhussain@gmail.com>
2015-03-27 12:49:13 +0500

    Fixed admin view

--- app/overrides/add_sale_price_to_product_edit.rb
+++ app/overrides/add_sale_price_to_product_edit.rb
@@ -1,11 +1,15 @@
 Deface::Override.new(virtual_path: 'spree/admin/products/_form',
   name: 'add_sale_price_to_product_edit',
-  insert_after: "erb[loud]:contains('text_field :price')",
+  insert_after: "div[data-hook='admin_product_form_price']",
   text: "
-    <%= f.field_container :sale_price do %>
+  <div data-hook='admin_product_form_sale_price'>
+    <%= f.field_container :sale_price, class: ['form-group']  do %>
       <%= f.label :sale_price, raw(Spree.t(:sale_price)) %>
-      <%= f.text_field :sale_price, value: number_to_currency(@product.sale_price, unit: '') %>
+      <%= f.text_field :sale_price, value: number_to_currency(@product.sale_price, unit: ''),class: 'form-control' %>
       <%= f.error_message_on :sale_price %>
     <% end %>
+</div>
+
   "
 )
+
